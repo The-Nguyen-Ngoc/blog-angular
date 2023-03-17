7ded693a07d73eda60dddf34731b63b7bb8f2b6d
thenn <thenn@onepay.vn>
2023-03-17 16:42:39 +0700

    base giao dien

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './page/home/home.component';
+import { CourseComponent } from './page/course/course.component';
+import { PostsComponent } from './page/posts/posts.component';
+import { AboutComponent } from './page/about/about.component';
+
+//Khai báo một constant chứa các route của app
+const routes: Routes = [
+  // { path: '', component: PageComponentComponent },
+  // { path: 'contact', component: ContactComponentComponent },
+  // { path: 'product', component: ProductComponentComponent },
+  { path: '', component: HomeComponent },
+  { path: 'course', component: CourseComponent },
+  { path: 'posts', component: PostsComponent },
+  { path: 'about', component: AboutComponent },
+  // { path: 'cart', component: CartComponentComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DependentComponent } from './page/dependent/dependent.component';
+import { HeaderComponent } from './page/header/header.component';
+import { FooterComponent } from './page/footer/footer.component';
+import { HomeComponent } from './page/home/home.component';
+import { CourseComponent } from './page/course/course.component';
+import { PostsComponent } from './page/posts/posts.component';
+import { AboutComponent } from './page/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DependentComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    CourseComponent,
+    PostsComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
